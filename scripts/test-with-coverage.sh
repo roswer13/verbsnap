09c8398d1170d45801e074c30c95f931947c86c9
Roswer García <roswer13@gmail.com>
2025-08-20 16:44:35 +0200

    fix: add logging for project and package paths in test coverage script

--- scripts/test-with-coverage.sh
+++ scripts/test-with-coverage.sh
@@ -8,6 +8,12 @@ PACKAGE_NAME=$3
 PACKAGE_LCOV_INFO_PATH=$PROJECT_ROOT_PATH/coverage/lcov_$PACKAGE_NAME.info
 PACKAGE_TEST_REPORT_PATH=$PROJECT_ROOT_PATH/test_reports/${PACKAGE_NAME}_test_report.json
 
+echo "Project root path: $PROJECT_ROOT_PATH"
+echo "Package path: $PACKAGE_PATH"
+echo "Running tests for package: $PACKAGE_NAME"
+echo "LCOV info path: $PACKAGE_LCOV_INFO_PATH"
+echo "Test report path: $PACKAGE_TEST_REPORT_PATH"
+
 mkdir -p $PROJECT_ROOT_PATH/coverage/
 flutter test \
   --no-pub \
@@ -15,6 +21,7 @@ flutter test \
   --coverage \
   --coverage-path $PACKAGE_LCOV_INFO_PATH > $PACKAGE_TEST_REPORT_PATH
 
+echo "Test report generated at: $PACKAGE_TEST_REPORT_PATH"
 escapedPath="$(echo $PACKAGE_PATH | sed 's/\//\\\//g')"
 
 # Requires gsed on MacOS machines because otherwise sed is not the same...
